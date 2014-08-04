80f1febffcbdf1a76b64f366b375ea140b409956
yinglovezhuzhu <yinglovezhuzhu@139.com>
2014-08-04 10:37:47 +0800

    Tow menu demo

--- SliddingMenu/src/com/opensource/slidingmenu/SlidingMenu.java
+++ SliddingMenu/src/com/opensource/slidingmenu/SlidingMenu.java
@@ -18,7 +18,6 @@ import android.os.Handler;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
